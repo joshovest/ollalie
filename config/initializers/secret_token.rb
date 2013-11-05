b1485b3f49949c8a440b7a257955d903838d2b36
Josh West <joshovest@gmail.com>
2013-11-05 10:59:49 -0700

    heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ollalie::Application.config.secret_token = '4c4cd24538e21384a6d22fe2e4a8118396f95e83e38fd0b17e561c52dc1099dc673da2f8b09f0e612847ebdb8a2b0f86b4e7840b15b39e80ad59c40bd4f30c9f'
